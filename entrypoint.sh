b63e98ad3bc2cfbbae46f841632f75e3510f80d4
diego <diegorodrigues-5@hotmail.com>
2023-09-05 21:29:15 -0300

    :bug: Corrigindo problema do sensor de temperatura

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-# Aplica o template
-# sudo influx apply -u https://raw.githubusercontent.com/influxdata/community-templates/master/raspberry-pi/raspberry-pi-system.yml --org hydroponics --token FRM0mVp2ty-IJJebPp-Qy2V67ahAZch6LPDsZdF5irNrjicu2wtO1PfdWgju86AMQwA-jFpmnYxXWBvyjTPm1w==
+# Protects script from continuing with an error
+set -eu -o pipefail
+
+# Ensures environment variables are set
+export DOCKER_INFLUXDB_INIT_MODE=$DOCKER_INFLUXDB_INIT_MODE
+export DOCKER_INFLUXDB_INIT_USERNAME=$DOCKER_INFLUXDB_INIT_USERNAME
+export DOCKER_INFLUXDB_INIT_PASSWORD=$DOCKER_INFLUXDB_INIT_PASSWORD
+export DOCKER_INFLUXDB_INIT_ORG=$DOCKER_INFLUXDB_INIT_ORG
+export DOCKER_INFLUXDB_INIT_BUCKET=$DOCKER_INFLUXDB_INIT_BUCKET
+export DOCKER_INFLUXDB_INIT_RETENTION=$DOCKER_INFLUXDB_INIT_RETENTION
+export DOCKER_INFLUXDB_INIT_ADMIN_TOKEN=$DOCKER_INFLUXDB_INIT_ADMIN_TOKEN
+export DOCKER_INFLUXDB_INIT_PORT=$DOCKER_INFLUXDB_INIT_PORT
+export DOCKER_INFLUXDB_INIT_HOST=$DOCKER_INFLUXDB_INIT_HOST
+
+# Conducts initial InfluxDB using the CLI
+influx setup --skip-verify --bucket ${DOCKER_INFLUXDB_INIT_BUCKET} --retention ${DOCKER_INFLUXDB_INIT_RETENTION} --token ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN} --org ${DOCKER_INFLUXDB_INIT_ORG} --username ${DOCKER_INFLUXDB_INIT_USERNAME} --password ${DOCKER_INFLUXDB_INIT_PASSWORD} --host http://${DOCKER_INFLUXDB_INIT_HOST}:8086 --force
