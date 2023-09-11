4569242dac17c5eda384275593e881bd0bc02909
diego <diegorodrigues-5@hotmail.com>
2023-09-10 22:30:43 -0300

    :hammer: Configurando script do mqtt para configurar usuário e senha

--- mosquitto.sh
+++ mosquitto.sh
@@ -3,10 +3,17 @@
 # Protects script from continuing with an error
 set -eu -o pipefail
 
-# Create passwd empty file
-touch /mosquitto/config/passwd
+# Path to the passwd file
+PASSWD_FILE="/mosquitto/config/passwd"
 
-mosquitto_passwd -b /mosquitto/config/passwd ${MQTT_USER_NAME} ${MQTT_PASSWORD}
+# Check if the file doesn't exist or is empty
+if [ ! -s "$PASSWD_FILE" ]; then
+    # Create or empty the passwd file
+    > "$PASSWD_FILE"
+
+    # Add the user and password
+    mosquitto_passwd -b "$PASSWD_FILE" "${MQTT_USER_NAME}" "${MQTT_PASSWORD}"
+fi
 
 # Start Mosquitto
 exec /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf
